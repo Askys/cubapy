6821b4cdde8e79ac26d3e3f9d801d34a8b1f9356
Adam <Adamche@protonmail.com>
2023-03-13 16:34:43 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    packages=[
+        'cubapy',
+    ],
+    data_files=[('licenses', ['MIT']), ('README', ['README.md'])],
+    install_requires=[
+        'numpy'
+        'plotly'
+    ]
+)
---
+++ test.py
@@ -0,0 +1,10 @@
+from cubapy import *
+
+pts = line(2)
+plot_pts(*pts)
+
+pts = tri(3)
+plot_pts(*pts)
+
+pts = tet(4)
+plot_pts(*pts)
